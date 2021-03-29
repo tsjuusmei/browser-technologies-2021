3fe88b2abfe9e3e42f3735965a53063b6dcce016
tsjuusmei <guusmay2@hotmail.com>
2021-03-29 21:57:31 +0200

    added partial form validation

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,13 @@
 console.log('js is loaded')
+
+function validateForm() {
+  const name = document.forms["login"]["name"].value;
+  const studentNumber = document.forms["login"]["user_studentnr"].value
+  if (name == "") {
+    alert("Vul je naam in");
+    return false;
+  } else if (studentNumber == ""){
+    alert("Vul je studentnummer in");
+    return false;
+  }
+}
